5d097eb0f2c293ec400ba07cbc08c4bd5d45c830
Welcome to Diya Maity's profile! <116588611+diya-2003@users.noreply.github.com>
2023-06-30 01:10:45 +0530

    Update temparature_converter.java
    The Temperature Converter Desktop Application is a Java-based graphical user interface (GUI) application that allows users to convert temperatures between Celsius, Fahrenheit, and Kelvin scales. This application provides a simple and intuitive interface for users to input a temperature value, select the desired conversion unit, and instantly obtain the converted temperature.
    
    Features:
    1.User-friendly Interface: The application offers an intuitive and easy-to-use graphical interface that allows users to interact with the conversion functionality effortlessly.
    
    2.Temperature Conversion Options: Users can convert temperatures between Celsius, Fahrenheit, and Kelvin scales by selecting the desired unit from a dropdown menu.
    
    3.Real-time Conversion: The application provides instant and accurate temperature conversions as soon as the user enters the temperature value and selects the conversion unit.
    
    4.Error Handling: The application incorporates error handling to ensure that valid temperature values are entered. It displays error messages if the input is invalid or contains more than two digits after the decimal point.
    
    4.Clear Functionality: A "Clear" button is provided to allow users to reset the input fields and the result area, enabling them to perform a new conversion easily.
    
    Usage:
    1.Launch the Application: Run the Java program to launch the Temperature Converter application.
    
    2.nput Temperature: Enter the temperature value in the "Enter temperature" text field.
    
    3.Select Conversion Unit: Choose the desired temperature unit (Celsius, Fahrenheit, or Kelvin) from the dropdown menu.
    
    4.Perform Conversion: Click the "Convert" button to initiate the temperature conversion process.
    
    5.View Result: The converted temperature will be displayed in the "Result" text field.
    
    6.Clear Fields: To perform a new conversion, click the "Clear" button to reset the input fields and the result area.
    
    This Temperature Converter Desktop Application is an efficient and convenient tool for users who frequently need to convert temperatures between different scales.

--- temparature_converter.java
+++ temparature_converter.java
@@ -1,1 +1,123 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+class p2 extends JFrame
+{
+     Container c;
+     JLabel l1,l2,l3;
+     JTextField t1,t2;
+     JComboBox<String> cbox;
+     JButton b1,b2;
+
+    public p2()
+    {
+        setTitle("Temperature Converter");
+        setSize(400, 300);
+        setLocation(200, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        c=getContentPane();
+        c.setLayout(null);
+        component();
+    }
+    public void component()
+    {
+        l3 = new JLabel("Result");
+        l3.setBounds(20, 150, 100, 20);
+        c.add(l3);
+
+        l1 = new JLabel("Enter temperature");
+        l1.setBounds(20, 50, 200, 20);
+        c.add(l1);
+
+        l2 = new JLabel("Convert temperature");
+        l2.setBounds(20, 100, 200, 20);
+        c.add(l2);
+
+       t1=new JTextField();
+       t1.setBounds(180,50,120,20);
+       c.add(t1);
+
+       t2=new JTextField();
+        t2.setBounds(180, 150, 180, 20);
+        t2.setEditable(false);
+       c.add(t2);
+
+       String[] s={"Celsius","Fahrenheit","Kelvin"};
+       cbox=new JComboBox<String>(s);
+       cbox.setBounds(180, 100, 100, 20);
+       c.add(cbox);
+
+       b1=new JButton("Clear");
+        b1.setBounds(20, 200, 100, 30);
+       c.add(b1);
+       b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) 
+            {
+                t1.setText("");
+                t2.setText("");
+            }});
+
+       b2=new JButton("Convert");
+       b2.setBounds(140, 200, 100, 30);
+       c.add(b2);   
+       b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e)
+            {
+                try {
+                convertTemperature();
+                }catch (NumberFormatException ex) 
+                {
+                    JOptionPane.showMessageDialog(null, "Please enter a valid number");
+                }catch (Exception ex) 
+                {
+                    JOptionPane.showMessageDialog(null, "Please enter a number with no more than 2 digits after the decimal point");
+                }  
+       }});
+
+       setVisible(true);
+    }
+    private void convertTemperature() throws Exception{
+        String input = t1.getText();
+        String unit = (String) cbox.getSelectedItem();
+        double temperature = Double.parseDouble(input);
+        double convertedTemperature = 0;
+        switch (unit) 
+            {
+             case "Celsius":
+                convertedTemperature = celsiusToFahrenheit(temperature);
+             break;
+             case "Fahrenheit":
+                convertedTemperature = fahrenheitToCelsius(temperature);
+             break;
+             case "Kelvin":
+                convertedTemperature = kelvinToCelsius(temperature);
+             break;
+            }
+
+            if (input.contains(".") && input.split("\\.")[1].length() > 2) 
+            {
+                JOptionPane.showMessageDialog(null, "Please enter a number with no more than 2 digits after the decimal point");
+                return;
+            }
+            t2.setText("" + convertedTemperature);
+    }
+
+    private double celsiusToFahrenheit(double celsius) 
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+    private double fahrenheitToCelsius(double fahrenheit) 
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+    private double kelvinToCelsius(double kelvin) 
+    {
+        return kelvin - 273.15;
+    }
+}
+class project2{
+public static void main(String[] args) 
+{
+    p2 p=new p2();
+}}
